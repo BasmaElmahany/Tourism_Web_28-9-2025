3f73a0926a352a8b3774a645ad3cddfa3cdf567c
Basma Elmahany <basmakhalaf974@gmail.com>
2025-10-05 11:51:25 +0300

    edit

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -13,7 +13,7 @@ import { blogPosts } from 'src/app/data/blog_posts';
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss'],
-  standalone: true,
+
   imports: [CommonModule, RouterModule, FormsModule],
 
 })
